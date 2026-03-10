5eb50b3f9b324bab006b42e2e468b95c30989099
Jim Petersen <jpeter20@gmail.com>
2026-03-09 22:07:30 -0400

    Use stdin instead of a static filename

--- Python/jimp/day1_pt1_2025.py
+++ Python/jimp/day1_pt1_2025.py
@@ -1,19 +1,20 @@
-with open("input.txt", "r") as f:
-    pos = 50
-    count = 0
-    for line in f:
-        letter = line[0]
-        numbers = int(line[1:])
-        print("Letter: " + letter + ", numbers: " + str(numbers))
+import sys
 
-        match letter:
-            case 'L':
-                dir = -1
-            case 'R':
-                dir = 1
+pos = 50
+count = 0
+for line in sys.stdin:
+    letter = line[0]
+    numbers = int(line[1:])
+    print("Letter: " + letter + ", numbers: " + str(numbers))
 
-        pos = (pos + dir*numbers) % 100
-        if pos == 0:
-            count += 1
-        print("New position:", pos, ", count =", count)
+    match letter:
+        case 'L':
+            dir = -1
+        case 'R':
+            dir = 1
+
+    pos = (pos + dir*numbers) % 100
+    if pos == 0:
+        count += 1
+    print("New position:", pos, ", count =", count)
 
--- Python/jimp/day1_pt2_2025.py
+++ Python/jimp/day1_pt2_2025.py
@@ -1,46 +1,47 @@
-with open("input.txt", "r") as f:
-    newpos = 50
-    count = 0
-    for line in f:
-        line = line.rstrip()
-        oldpos = newpos
-        letter = line[0]
-        move = int(line[1:])
-        if move == 0:
-            continue
+import sys
 
-        match letter:
-            case 'L':
-                dir = -1
-            case 'R':
-                dir = 1
+newpos = 50
+count = 0
+for line in sys.stdin:
+    line = line.rstrip()
+    oldpos = newpos
+    letter = line[0]
+    move = int(line[1:])
+    if move == 0:
+        continue
 
-        print("Line:", line, ", old pos:", oldpos, end='')
+    match letter:
+        case 'L':
+            dir = -1
+        case 'R':
+            dir = 1
 
-        # Calculate number of full wrap-arounds; each passes 0 again
-        wraps = move // 100
-        if wraps >= 1:
-            print(", wrap", wraps, end='')
-        count += wraps
+    print("Line:", line, ", old pos:", oldpos, end='')
 
-        # Calculate remaining moves
-        move -= wraps*100
+    # Calculate number of full wrap-arounds; each passes 0 again
+    wraps = move // 100
+    if wraps >= 1:
+        print(", wrap", wraps, end='')
+    count += wraps
 
-        # Get proper direction for move
-        move *= dir
+    # Calculate remaining moves
+    move -= wraps*100
 
-        # Move the partial wrap-around
-        newpos = oldpos + move
+    # Get proper direction for move
+    move *= dir
 
-        # See if we went off the edge, and increment count if so,
-        # but only if we didn't start at 0 (prevent double-count).
-        if (newpos != newpos % 100):
-            newpos = newpos % 100
-            if oldpos != 0:
-                count += 1
-        # Count landing on 0 once.
-        elif newpos == 0:
+    # Move the partial wrap-around
+    newpos = oldpos + move
+
+    # See if we went off the edge, and increment count if so,
+    # but only if we didn't start at 0 (prevent double-count).
+    if (newpos != newpos % 100):
+        newpos = newpos % 100
+        if oldpos != 0:
             count += 1
-        print(", new pos:", newpos, ", count =", count, "move:", move)
+    # Count landing on 0 once.
+    elif newpos == 0:
+        count += 1
+    print(", new pos:", newpos, ", count =", count, "move:", move)
 
 print("Final result:", count)
